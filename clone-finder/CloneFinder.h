b72796ea0f38f5b0301f91e934d2549c22e12225
Raphael Isemann <teemperor@gmail.com>
2016-03-25 17:16:54 +0100

    removed scan-build replacement, moved to compilation database to make things easier

--- clone-finder/CloneFinder.h
+++ clone-finder/CloneFinder.h
@@ -2,8 +2,85 @@
 #define STANDALONE_CLONEFINDER_H
 
 
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Driver/Compilation.h"
+#include "clang/Driver/Driver.h"
+#include "clang/Driver/Tool.h"
+#include "clang/Driver/ToolChain.h"
+#include "clang/Frontend/ASTUnit.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendDiagnostic.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "clang/Tooling/ArgumentsAdjusters.h"
+#include "clang/Tooling/CompilationDatabase.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/Config/llvm-config.h"
+#include "llvm/Option/Option.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Support/raw_ostream.h"
+#include "clang/Serialization/ASTWriter.h"
+#include "clang/Serialization/ASTReader.h"
+
+using namespace clang;
+using namespace clang::vfs;
+using namespace clang::tooling;
+using namespace llvm;
+
+
+class FindNamedClassVisitor
+        : public RecursiveASTVisitor<FindNamedClassVisitor> {
+    
+public:
+    explicit FindNamedClassVisitor(ASTContext *Context)
+            : Context(Context) {}
+
+    bool VisitStmt(Stmt *S) {
+        FoldingSetNodeID id;
+        S->Profile(id, *Context, false);
+        unsigned hash = id.ComputeHash();
+
+        if (hashes.find(hash) == hashes.end()) {
+            hashes[hash] = S;
+        } else {
+            std::cerr << "\n\n\n\n\n############################\nFound clone:" << std::endl;
+            hashes[hash]->getLocStart().dump(Context->getSourceManager());
+            std::cerr << std::endl;
+            hashes[hash]->dump();
+            std::cerr << "\n############################" << std::endl;
+            S->getLocStart().dump(Context->getSourceManager());
+            std::cerr << std::endl;
+            S->dump();
+            std::cerr << "\n############################" << std::endl;
+        }
+
+        return true;
+    }
+
+private:
+    ASTContext *Context;
+    std::map<unsigned, Stmt*> hashes;
+};
+
+
 class CloneFinder {
 
+public:
+    void searchAST(std::vector<ASTUnit*>& asts) {
+        for (ASTUnit* ast : asts) {
+            FindNamedClassVisitor visitor(&ast->getASTContext());
+            visitor.TraverseTranslationUnitDecl(ast->getASTContext().getTranslationUnitDecl());
+        }
+    }
 };
 
 
--- clone-finder/main.cpp
+++ clone-finder/main.cpp
@@ -1,61 +1,69 @@
-#include <fstream>
+// Declares clang::SyntaxOnlyAction.
 #include <iostream>
-#include "InterProcessArgs.h"
-#include "CCXXEnvOverwriter.h"
-#include "FakeCCListener.h"
+#include <fstream>
+#include <sstream>
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
+#include "CloneFinder.h"
 
+using namespace clang::tooling;
+using namespace llvm;
 
-int main(int argc, char **argv) {
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static cl::OptionCategory MyToolCategory("clang-clone-finder options");
 
-    std::string fakeccPath = argv[0] + std::string("-fakecc");
+std::string getFileContents(const std::string& path) {
+    std::ifstream inputStream(path);
+    std::stringstream buffer;
+    buffer << inputStream.rdbuf();
+    return buffer.str();
+}
 
-    CCXXEnvOverwriter ccxxEnvOverwriter(fakeccPath);
+bool hasEnding (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
 
-    std::vector<std::string> args;
-    for (int i = 0; i < argc; i++) {
-        args.push_back(argv[i]);
+int main(int argc, const char **argv) {
+    CommonOptionsParser OptionsParser(argc, argv, MyToolCategory);
+
+    std::vector<clang::ASTUnit*> asts;
+
+    for (CompileCommand s : OptionsParser.getCompilations().getAllCompileCommands()) {
+
+        std::string filename;
+
+        std::string allArgs = "";
+
+        for (std::string arg : s.CommandLine) {
+            if (hasEnding(arg, ".cpp") || hasEnding(arg, ".cc") || hasEnding(arg, ".c") || hasEnding(arg, ".cxx")) {
+                filename = arg;
+            }
+        }
+
+        s.CommandLine.erase(std::find(s.CommandLine.begin(), s.CommandLine.end(), filename));
+
+        std::string code = getFileContents(filename);
+
+        auto ast = clang::tooling::buildASTFromCodeWithArgs(code, s.CommandLine, filename);
+
+        assert(ast.get());
+        asts.push_back(ast.release());
     }
 
-    std::stringstream command;
+    CloneFinder cloneFinder;
+    cloneFinder.searchAST(asts);
 
-    for (std::size_t i = 1; i < args.size(); i++) {
-        command << "\"" << argv[i] << "\" ";
-    }
+    /*clang::tooling::buildASTFromCodeWithArgs();
 
-
-    FakeCCListener listener;
-
-    std::cout << "Running  '" << command.str() << "'" << std::endl;
-
-    FILE* childProcess = popen(command.str().c_str(), "r");
-
-    if (childProcess == NULL) {
-        std::cerr << "error with popen\n";
-        return 1;
-    }
-
-    /*
-    char c;
-    do {
-        c = (char) fgetc(childProcess);
-        std::cout << c;
-    } while (c != EOF);
-    */
-
-    listener.run();
-
-
-    std::cout << "finished" << std::endl;
-
-    pclose(childProcess);
-
-#if false
-    Twine fileName = argv[i];
-    std::string source = getFileContents(argv[i]);
-
-    std::shared_ptr<ASTUnit> ast = clang::tooling::buildASTFromCode(source, fileName);
-
-    FindNamedClassVisitor visitor(&ast->getASTContext());
-    visitor.TraverseTranslationUnitDecl(ast->getASTContext().getTranslationUnitDecl());
-#endif
+    ClangTool Tool(OptionsParser.getCompilations(),
+                   OptionsParser.getSourcePathList());
+    return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get()); */
 }
